454fc6582dee1c62511b6329041ce0ce1b6c2f26
Luz <779410241@qq.com>
2022-06-02 10:37:31 +0800

    Update hic_pileup.py

--- hic_pileup.py
+++ hic_pileup.py
@@ -3,56 +3,68 @@
 ## Pileups is beneficail for reliably observing features in low-coverage Hi-C or single-cell HiC maps
 ## On-diagonal pileup & Off-diagonal pileup
 
+############# h4k20me1_hic_pileup.py ################
 ## import packages
 import cooler
 import bioframe
+import pandas as pd
 import numpy as np
 import cooltools
 import matplotlib.pyplot as plt
+import argparse
+
+## parse parameters
+parser = argparse.ArgumentParser(description = 'Hi-C pileup heatmap')
+parser.add_argument('-S', '--sample', help = 'sample name')
+args = parser.parse_args()
+sample = args.sample
 
 ## Load data
-clr = cooler.Cooler('W_rep1.multires.cool::/resolutions/10000')
-resolution = clr.binsize
+clr = cooler.Cooler(sample + '.multires.cool::/resolutions/10000')
+resolution = 10_000
 
-## Fetch and select genomic features
-mm10_chromsizes = bioframe.fetch_chromsizes('mm10')
-mm10_cens = bioframe.fetch_centromeres('mm10')  # empty DataFrame
-mm10_arms = bioframe.make_chromarms(mm10_chromsizes, mm10_cens)
-mm10_arms = mm10_arms[mm10_arms.chrom.isin(clr.chromnames)].reset_index(drop = True)
-mm10_arms = mm10_arms[mm10_arms['chrom'] != 'chrM']  # chrM is too short
+## Fetch and select genomic features (need internet)
+#mm10_chromsizes = bioframe.fetch_chromsizes('mm10')
+#mm10_cens = bioframe.fetch_centromeres('mm10')  # empty DataFrame
+#mm10_arms = bioframe.make_chromarms(mm10_chromsizes, mm10_cens)
+#mm10_arms = mm10_arms[mm10_arms.chrom.isin(clr.chromnames)].reset_index(drop = True)
+#mm10_arms = mm10_arms[mm10_arms['chrom'] != 'chrM']  # chrM is too short
+#mm10_arms.to_csv('mm10_arms.csv', index = False)
+mm10_arms = pd.read_csv('mm10_arms.csv')
 
 ## Load features for anchors
 h4k20me1 = bioframe.read_table('WH4K20_10kb.bedgraph')
 h4k20me1.columns = ['chrom', 'start', 'end', 'score']
-h4k20me1 = h4k20me1.query(f'chrom in {clr.chromnames}')
+h4k20me1 = h4k20me1.query(f'chrom in {wt_clr.chromnames}')
 h4k20me1['mid'] = (h4k20me1.end + h4k20me1.start) // 2
 
-## Select sites (top 20%)
-sites = h4k20me1[h4k20me1['score'] >= np.percentile(h4k20me1['score'], 99)]
+## Select sites (high & mid & low)
+hig = h4k20me1[h4k20me1['score'] >= np.percentile(h4k20me1['score'], 95)]
+mid = h4k20me1[(h4k20me1['score'] >= np.percentile(h4k20me1['score'], 47.5)) &
+               (h4k20me1['score'] <= np.percentile(h4k20me1['score'], 52.5))]
+low = h4k20me1[h4k20me1['score'] <= np.percentile(h4k20me1['score'], 5)]
+#sites = bioframe.cluster(sites, min_dist = resolution * 5).drop_duplicates('cluster').reset_index(drop = True)
+# results from filteration and unfilteration are almost identity
+
 
 ## On-diagonal pileup of observed over expected interactions
 expected = cooltools.expected_cis(clr, view_df = mm10_arms, nproc = 20, chunksize = 1_000_000)
-stack = cooltools.pileup(clr, sites, view_df = mm10_arms, expected_df = expected, flank = 300_000)
-mtx = np.nanmean(stack, axis = 2)
 
-## Plot heatmap
-flank = 300_000
-plt.imshow(
-    np.log2(mtx),
-    vmax = 0.25,
-    vmin = -0.25,
-    cmap='coolwarm',
-    interpolation='none')
-
-plt.colorbar(label = 'log2 mean obs/exp')
-ticks_pixels = np.linspace(0, flank*2//resolution,5)
-ticks_kbp = ((ticks_pixels-ticks_pixels[-1]/2)*resolution//1000).astype(int)
-plt.xticks(ticks_pixels, ticks_kbp)
-plt.yticks(ticks_pixels, ticks_kbp)
-plt.xlabel('relative position, kbp')
-plt.ylabel('relative position, kbp')
-
-plt.savefig('W_rep1_h4k20me1.pdf')
-plt.close()
-
-
+for lev in [hig, mid, low]:
+    stack = cooltools.pileup(clr, lev, view_df = mm10_arms, expected_df = expected, flank = 500_000, nproc = 20)
+    mtx = np.nanmean(stack, axis = 2)
+    
+    ## Plot heatmap
+    flank = 500_000
+    plt.imshow(np.log2(mtx), vmax = -0.1, vmin = 0.1, cmap = 'coolwarm', interpolation='none')
+    plt.colorbar(label = 'log2 mean obs/exp')
+    ticks_pixels = np.linspace(0, flank*2//resolution,5)
+    ticks_kbp = ((ticks_pixels-ticks_pixels[-1]/2)*resolution//1000).astype(int)
+    plt.xticks(ticks_pixels, ticks_kbp)
+    plt.yticks(ticks_pixels, ticks_kbp)
+    plt.xlabel('relative position, kbp')
+    plt.ylabel('relative position, kbp')
+    plt.savefig(sample + '_h4k20me1_' + lev '_pileup.pdf')
+    plt.close()
+    
+    print(sample, lev, 'done')
