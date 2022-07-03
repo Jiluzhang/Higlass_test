f00896ed1338b12a90b034de0282c87c594588f8
Luz <779410241@qq.com>
2022-07-03 11:23:23 +0800

    Update oe_curve.py

--- oe_curve.py
+++ oe_curve.py
@@ -3,7 +3,6 @@
 ## Pileups is beneficail for reliably observing features in low-coverage Hi-C or single-cell HiC maps
 ## On-diagonal pileup & Off-diagonal pileup
 
-############# h4k20me1_hic_pileup.py ################
 ## import packages
 import cooler
 import bioframe
@@ -13,71 +12,69 @@ import cooltools
 import matplotlib.pyplot as plt
 import argparse
 import warnings
+from plotnine import *
 
 warnings.filterwarnings('ignore')
 
-## parse parameters
-parser = argparse.ArgumentParser(description = 'Hi-C pileup heatmap')
-parser.add_argument('-S', '--sample', help = 'sample name')
-args = parser.parse_args()
-sample = args.sample
+#bedtools makewindows -g ~/ref_genome/chrom_sizes/mm10.chrom.sizes -w 10000 > mm10_10kb.bed
+#bedtools intersect -a mm10_10kb.bed -b WH4K20_10kb.bedgraph -wa -wb | awk '{print $1 "\t" $2 "\t" $3 "\t" $7}' | \
+#awk '{if($3%10000==0) print$0}' > WH4K20_10kb_all.bedgraph   # pay attention to end of chromosome
 
-## Load data
-clr = cooler.Cooler(sample + '.multires.cool::/resolutions/10000')
 resolution = 10_000
 
-## Fetch and select genomic features (need internet)
-#mm10_chromsizes = bioframe.fetch_chromsizes('mm10')
-#mm10_cens = bioframe.fetch_centromeres('mm10')  # empty DataFrame
-#mm10_arms = bioframe.make_chromarms(mm10_chromsizes, mm10_cens)
-#mm10_arms = mm10_arms[mm10_arms.chrom.isin(clr.chromnames)].reset_index(drop = True)
-#mm10_arms = mm10_arms[mm10_arms['chrom'] != 'chrM']  # chrM is too short
-#mm10_arms.to_csv('mm10_arms.csv', index = False)
+## genomic features
 mm10_arms = pd.read_csv('mm10_arms.csv')
-#chrom,start,end,name
-#chr1,0,195471971,chr1_p
-#chr2,0,182113224,chr2_p
-#chr3,0,160039680,chr3_p
-#chr4,0,156508116,chr4_p
-#chr5,0,151834684,chr5_p
-
+chr = ['chr' + str(i) for i in range(1, 20)]
+chr.append('chrX')
 
 ## Load features for anchors
-h4k20me1 = bioframe.read_table('WH4K20_10kb.bedgraph')
+h4k20me1 = bioframe.read_table('WH4K20_10kb_all.bedgraph')
 h4k20me1.columns = ['chrom', 'start', 'end', 'score']
-h4k20me1 = h4k20me1.query(f'chrom in {clr.chromnames}')
+h4k20me1 = h4k20me1.query(f'chrom in {chr}')
 h4k20me1['mid'] = (h4k20me1.end + h4k20me1.start) // 2
+h4k20me1 = h4k20me1[h4k20me1['score'] !=0]
 
-## Select sites (high & mid & low)
-hig = h4k20me1[h4k20me1['score'] >= np.percentile(h4k20me1['score'], 95)]
-mid = h4k20me1[(h4k20me1['score'] >= np.percentile(h4k20me1['score'], 47.5)) &
-               (h4k20me1['score'] <= np.percentile(h4k20me1['score'], 52.5))]
-low = h4k20me1[h4k20me1['score'] <= np.percentile(h4k20me1['score'], 5)]
-## hig: 1.29~10.34  mid: 0.21~0.24  low: 0~0.04
-#sites = bioframe.cluster(sites, min_dist = resolution * 5).drop_duplicates('cluster').reset_index(drop = True)
-# results from filteration and unfilteration are almost identity
+## Select sites (high & low)
+hig = h4k20me1[h4k20me1['score'] >= np.percentile(h4k20me1['score'], 70)]
+low = h4k20me1[h4k20me1['score'] <  np.percentile(h4k20me1['score'], 70)]
+# 0.367184
 
+dict = {'high': hig, 'low': low}
 
-## On-diagonal pileup of observed over expected interactions
-expected = cooltools.expected_cis(clr, view_df = mm10_arms, nproc = 20, chunksize = 1_000_000)
-
-dict = {'high': hig, 'middle': mid, 'low': low}
-
-for lev in ['high', 'middle', 'low']:
-    stack = cooltools.pileup(clr, dict[lev], view_df = mm10_arms, expected_df = expected, flank = 500_000, nproc = 20)
-    mtx = np.nanmean(stack, axis = 2)
+def plot_oe_vs_dist(c, t):
+    wt_clr = cooler.Cooler(c + '.multires.cool::/resolutions/10000')
+    wt_expected = cooltools.expected_cis(wt_clr, view_df = mm10_arms, nproc = 40, chunksize = 1_000_000)
     
-    ## Plot heatmap
-    flank = 500_000
-    plt.imshow(np.log2(mtx), vmax = -0.1, vmin = 0.1, cmap = 'coolwarm', interpolation='none')
-    plt.colorbar(label = 'log2 mean obs/exp')
-    ticks_pixels = np.linspace(0, flank*2//resolution,5)
-    ticks_kbp = ((ticks_pixels-ticks_pixels[-1]/2)*resolution//1000).astype(int)
-    plt.xticks(ticks_pixels, ticks_kbp)
-    plt.yticks(ticks_pixels, ticks_kbp)
-    plt.xlabel('relative position, kbp')
-    plt.ylabel('relative position, kbp')
-    plt.savefig(sample + '_h4k20me1_' + lev + '_pileup.pdf')
-    plt.close()
+    mt_clr = cooler.Cooler(t + '.multires.cool::/resolutions/10000')
+    mt_expected = cooltools.expected_cis(mt_clr, view_df = mm10_arms, nproc = 40, chunksize = 1_000_000)
     
-    print(sample, lev, 'done')
+    for lev in ['high', 'low']:
+        paired_sites = bioframe.pair_by_distance(dict[lev], min_sep = 20_000, max_sep = 1_000_000, suffixes = ('1', '2'))
+        paired_sites.loc[:, 'mid1'] = (paired_sites['start1'] + paired_sites['end1'])//2
+        paired_sites.loc[:, 'mid2'] = (paired_sites['start2'] + paired_sites['end2'])//2
+        paired_sites['dist'] = abs(paired_sites['mid1'] - paired_sites['mid2'])
+        
+        wt_stack = cooltools.pileup(wt_clr, paired_sites, view_df = mm10_arms, expected_df = wt_expected, flank = 0, nproc = 40)
+        wt_tmp = paired_sites[['dist']]
+        wt_tmp['oe'] = wt_stack[0][0]
+        wt_res = wt_tmp[['dist', 'oe']].groupby('dist', as_index = False).mean()
+        wt_res['idx'] = 'wt'
+        
+        mt_stack = cooltools.pileup(mt_clr, paired_sites, view_df = mm10_arms, expected_df = mt_expected, flank = 0, nproc = 40)
+        mt_tmp = paired_sites[['dist']]
+        mt_tmp['oe'] = mt_stack[0][0]
+        mt_res = mt_tmp[['dist', 'oe']].groupby('dist', as_index = False).mean()
+        mt_res['idx'] = 'mt'
+        
+        res = pd.concat([wt_res, mt_res])
+        res['dist'] = res['dist'] / 1000000
+        
+        ## Plot heatmap
+        p = ggplot(res, aes(x = 'dist', y = 'oe', color = 'idx')) + geom_smooth() + \
+            scale_x_continuous(limits = [0, 1], breaks = np.arange(0, 1+0.01, 0.1)) + \
+            scale_y_continuous(limits = [0, 1.5], breaks = np.arange(0, 1.5+0.3, 0.3)) + theme_bw()
+        p.save(c + '_' + t + '_h4k20me1_' + lev + '_oe_vs_dist.pdf')
+        
+        print(c, t, lev, 'h4k20me1 done')
+
+plot_oe_vs_dist(c = 'W1G1', t = 'R1G1')
